d273a6b8f2c6547922f6334a7b2a2dcf29c73cfb
Rody Vilchez <102562850+R0SEWT@users.noreply.github.com>
2026-01-22 01:30:52 -0500

    fischl 1

--- syncwithsv.sh
+++ syncwithsv.sh
@@ -1,2 +1,5 @@
-scp -r /home/rosewt-dell/Documentos/potato-achis \
-    giorgio6846@172.25.130.175:/home/giorgio6846/Code/
+
+rsync -avz \
+  --exclude-from=.rsyncignore \
+  /home/rosewt-dell/Documentos/potato-achis/ \
+  giorgio6846@172.25.130.175:/home/giorgio6846/Code/potato-achis/
